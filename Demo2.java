68b59b83c3656879f357407b9b9f41d0a8359565
BUDAPEST\Yavor_Tsanov <yavor_tsanov@epam.com>
2016-10-04 10:40:01 +0300

    http client

--- Demo2.java
+++ Demo2.java
@@ -7,15 +7,24 @@ import Epam.dialogs.BaseModalDialog;
 import Epam.model.HistoryPage;
 import Epam.model.NewRequestPage;
 import Epam.model.contextable.HitsoryItem;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.util.List;
 
 /**
  * Created by Yavor_Tsanov on 10/4/2016.
@@ -24,6 +33,7 @@ public class Demo2 {
 
 
     public WebDriver driver;
+    private int invalidLinksCount;
 
 
     @BeforeMethod
@@ -33,6 +43,8 @@ public class Demo2 {
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+
+        driver.get("http://google.com");
 
 
     }
@@ -80,6 +92,61 @@ public class Demo2 {
 //        #########################################
 
     }
+
+
+    @Test
+    public void validateInvalidLinks() {
+
+        try {
+            invalidLinksCount = 0;
+            List<WebElement> anchorTagsList = driver.findElements(By
+                    .tagName("a"));
+            System.out.println("Total no. of links are "
+                    + anchorTagsList.size());
+            for (WebElement anchorTagElement : anchorTagsList) {
+                if (anchorTagElement != null) {
+                    String url = anchorTagElement.getAttribute("href");
+                    if (url != null && !url.contains("javascript")) {
+                        verifyURLStatus(url);
+                    } else {
+                        invalidLinksCount++;
+                    }
+                }
+            }
+
+            System.out.println("Total no. of invalid links are "
+                    + invalidLinksCount);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null)
+            driver.quit();
+    }
+
+    public void verifyURLStatus(String URL) {
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(URL);
+        try {
+            HttpResponse response = client.execute(request);
+            // verifying response code and The HttpStatus should be 200 if not,
+            // increment invalid link count
+            ////We can also check for 404 status code like response.getStatusLine().getStatusCode() == 404
+            if (response.getStatusLine().getStatusCode() != 200)
+                invalidLinksCount++;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 
 
 }
